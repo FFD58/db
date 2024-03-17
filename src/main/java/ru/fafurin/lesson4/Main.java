e7516e4e5d29921d5bd360746be0f92354dbf7d5
Fafurin <ffafurin@mail.ru>
2024-03-17 17:21:31 +0300

    Homework for lesson 4 is completed

--- src/main/java/ru/fafurin/lesson4/Main.java
+++ src/main/java/ru/fafurin/lesson4/Main.java
@@ -1,4 +1,7 @@
 package ru.fafurin.lesson4;
+
+import java.sql.*;
+
 // Урок 4. PostgreSQL
 public class Main {
     public static void main(String[] args) {
@@ -21,5 +24,49 @@ public class Main {
         // Ответ:
         // SELECT employee_id, first_name, last_name FROM Employees ORDER BY id DESC;
 
+
+
+
+
+
+
+
+        Connect connect = new Connect();
+        connect.connect();
+
+    }
+    static class Select {
+        private final Connect connect;
+
+        public Select(Connect connect) {
+            this.connect = connect;
+        }
+
+        public void select(String sql) {
+            try (Connection connection = connect.connect();
+                 Statement statement = connection.createStatement();
+                 ResultSet resultSet = statement.executeQuery(sql)) {
+
+                while (resultSet.next()) {
+                    System.out.println(resultSet.getString("first_name") + "\t" +
+                            resultSet.getString("last_name"));
+                }
+            } catch (SQLException e) {
+                throw new RuntimeException(e);
+            }
+        }
+    }
+
+    static class Connect {
+        private Connection connection;
+        public Connection connect() {
+            try {
+                connection = DriverManager.getConnection("jdbc:postgresql://localhost:5432/somedb", "someuser", "somepass");
+                System.out.println("Connection to SQLite has been established.");
+            } catch (SQLException e) {
+                System.out.println(e.getMessage());
+            }
+            return connection;
+        }
     }
 }
